2d0764aa3b78e2563896c92833a732444e46c6f4
keahi <trenstonricks@gmail.com>
2025-12-24 23:54:43 -1000

    Added v4l2 setup to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,43 @@
 # Jetson TX2 setup script
 
-set -e	# exit on error
+# exit on error
+set -e 
 
 echo "Starting NVIDIA Jetson TX2 setup..."
+echo
 
-# Step 1: Update system
-echo "Step 1: Updating system packages..."
+echo "Updating system packages..."
+echo
 apt update && apt upgrade -y
+
+echo "Installing dependencies..."
+echo
+apt install build-essentials git
+
+echo "Setting up v4l2loopback..."
+echo
+# v4l2loopback allows v4l2 to read from a "virtual device"
+# mimics a camera device
+# https://github.com/v4l2loopback/v4l2loopback?tab=readme-ov-file
+
+# may have issues with kernel headers (with Linux 4 Tegra)
+git clone "https://github.com/v4l2loopback/v4l2loopback.git"
+
+# build
+# build for a different kernel (maybe L4T): "$ make KERNELRELEASE=6.11.7-amd64"
+cd v4l2loopback
+make
+
+# install
+make && make install
+depmod -a
+
+# run
+modprobe v4l2loopback
+
+# test if device was created
+# ls -1 /sys/devices/virtual/video4linux
+# # video0 \ video3
+
+cd ~
+
