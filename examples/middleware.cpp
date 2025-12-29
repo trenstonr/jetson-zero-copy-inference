cb07ef1e7e6d367b1a8406c60daab516c0edbe5c
keahi <trenstonricks@gmail.com>
2025-12-28 21:52:30 -1000

    Outlined middleware pipeline in middleware.cpp

--- examples/middleware.cpp
+++ examples/middleware.cpp
@@ -3,7 +3,28 @@
 using namespace jetson_middleware;
 
 int main() {
-	V4L2Device dvc("C://abcd/aca", 256, 256);
+	// V4L2Device dvc(...);
+	//
+	// dvc.request_buffers(_);
+	// 
+	// dvc.start_stream();
+	// 
+	// while (running) {
+	//
+	// 	// dequeue filled frame
+	//
+	//	auto buf = dvc.dequeue_frame();
+	//
+	//	// process frame
+	//	
+	//	kernel<<<x,y>>>(...)
+	//
+	//	// enqueue empty frame
+	//	
+	//	dvc.enqueue_frame(buf.index);
+	// }
+	//
+	// dvc.stop_stream();
 
 	return 0;
 }
